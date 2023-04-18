3674c2d84f75f1dfbc52c01fa1c1c7193cd71340
Patricio Riffo <patoriffo55@gmail.com>
2023-04-17 22:42:22 -0400

    primer comit

---
+++ EJER 06 - java/Assets/JS/calculos1.js
@@ -0,0 +1,13 @@
+function calc-index (params) {console.log('sumapares'+);console.log('sumaimpares';)
+calc-index=sumapares/sumaimpares
+alert('indice calculado es:' calc-index)
+}
+function sumapares (n) {n=5
+    if (n<=0) return;
+console.log ('sumapares#'+n);n=5 sumapares(n-1);}
+
+function sumaimpares (n) {n=5
+    if (n<=0) return;
+    console.log ('sumaimpares#'+n);n=5 sumaimpares(n-1);}
+
+    
---
+++ EJER 06 - java/Assets/img/Js/funciones.js
@@ -0,0 +1,15 @@
+function calc-index (params) {console.log ('sumapares'+)
+ calc-index = sumapares / sumaimpares
+    console.log ('indice calculado'+)
+    alert("calculo index es"+ calc-index)
+    
+}
+function sumapares (n) {n=5
+if (n<=0) return;
+console.log ('sumapares#' +n); n=5
+sumapares(n-1);}
+
+function sumaimpares (n) {n=5
+if (n<=0) return;
+console.log ('sumaimpares#' +n); n=5
+sumapares(n-1);}
